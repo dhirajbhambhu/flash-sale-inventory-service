2cb0e153ad48b1b415900ab02862748d61f4e548
Dhiraj Bhambhu <dhirajbhambhu18@gmail.com>
2026-07-21 19:22:42 +0530

    Add Product entity, repository, and CRUD REST endpoints

---
+++ inventory-service/src/main/java/com/flashsale/inventory_service/Entity/Product.java
@@ -0,0 +1,28 @@
+package com.flashsale.inventory_service.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Integer totalStock;
+}
